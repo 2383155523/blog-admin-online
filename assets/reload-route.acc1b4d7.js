c181d4577823a713552fc3a04afd6eb0543c7647
微若蜉蝣 <58352334+2383155523@users.noreply.github.com>
2022-03-03 17:20:06 +0800

    Add files via upload

---
+++ assets/reload-route.acc1b4d7.js
@@ -0,0 +1,2 @@
+import{d as s,O as n,P as u,Q as a,g as c}from"./vendor.949f6402.js";const _=s({setup(p){const r=n(),t=u();return a(()=>{const{params:e}=r,{originUrl:o}=c(e);t.replace(o)}),(e,o)=>null}});export{_ as default};
+//# sourceMappingURL=reload-route.acc1b4d7.js.map
